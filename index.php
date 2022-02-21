bca03fa2a098ab919a5afa6cc206778799792e70
ozgurege0 <81050186+ozgurege0@users.noreply.github.com>
2022-02-21 20:58:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,6 @@ if (isset($_POST['submit'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
 
     <title>Youtube Video İndirici</title>
 </head>
